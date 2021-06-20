18d64da4da5e0034a6e25444eeb5611c5a21ac66
rferes1 <rferes@uc.cl>
2021-06-20 17:31:10 -0400

    arreglo modelo orden_compra_recibida

--- db/migrate/20210620212552_create_orden_compra_recibidas.rb
+++ db/migrate/20210620212552_create_orden_compra_recibidas.rb
@@ -2,13 +2,13 @@ class CreateOrdenCompraRecibidas < ActiveRecord::Migration[6.1]
   def change
     create_table :orden_compra_recibidas do |t|
       t.string :oc_id
-      t.string, :cliente
+      t.string :cliente
       t.bigint :sku
-      t.bigint :fechaEntrega
-      t.int :cantidad
+      t.datetime :fechaEntrega
+      t.integer :cantidad
       t.string :urlNotificacion
       t.string :estado
-      t.bigint :creacion
+      t.datetime :creacion
       t.boolean :en_despacho
 
       t.timestamps
