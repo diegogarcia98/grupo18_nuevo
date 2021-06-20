2ee894ca7392a4b22d2860fcd773455239769404
Diego García <dfgarcia1@uc.cl>
2021-06-20 18:30:30 -0400

    Actualizando rutas de OC y estado

--- app/controllers/orden_compra_controller.rb
+++ app/controllers/orden_compra_controller.rb
@@ -16,43 +16,42 @@ oc = Orden_compra_recibida.find_by(oc_id: params[:oc_id])
 =end
 
 class OrdenCompraController < ApplicationController
-  
-  def recibir
-  	url_api_oc = "https://dev.oc.2021-1.tallerdeintegracion.cl/oc"
-  	# se recibe el id de la orden de compra
-  	oc_id = params[:oc_id]
-  	# se revisa que no este duplicada FALTA
-  	if Orden_compra_recibida.exists?(oc_id: params[:oc_id])
-  		oc = Orden_compra_recibida.find_by(oc_id: params[:oc_id])
-    	render json: {mensaje: "OC ya fue recibida"},status: :bad_request
-  	# Si esta duplicada se responde con un error 400 Bad request y con un json con {"mensaje": "OC ya fue recibida"}
-
-
-  	# obtenemos url y se hace el request
-  	oc_hash = url_api_oc + "/obtener/" + oc_id
-  	url = url_api_oc + oc_hash
-  	@oc_json = HTTParty.get(url).parsed_response
-  	## se guarda la informacion en la bd
-  	fecha_entrega = oc_json['fechaEntrega'].to_i
-  	creada_en = Date.today.strftime("%Q").to_i
-  	# ejemplo para crear instancias en un modelo: artist = Artist.create(artist_id: artist_id, name: artist_name, age: artist_age, albums_url: albums_url, tracks_url: tracks_url, self: self_url)
-  	oc_nueva = Orden_compra_recibida.create(oc_id: oc_json['id'], cliente: oc_json['cliente'] , sku: oc_json['sku'], fechaEntrega: fecha_entrega, 
-  		cantidad: oc_json['cantidad'], urlNotificacion: oc_json['urlNotificacion'], estado: "recibida", creacion: creada_en , en_despacho: false)
-  	# se guadrda y se hace el response
-  	if oc_nueva.save
-        oc = Orden_compra_recibida.find_by(oc_id: params[:oc_id])
-    	render json: {id: oc.oc_id, 
-        cliente: oc.cliente, 
-        sku: oc.sku, 
-        fechaEntrega: oc.fechaEntrega,
-        cantidad: oc.cantidad,
-        urlNotificacion: oc.urlNotificacion,
-    	estado: oc.estado},status: :created
-      else
-        sts = :bad_request
-
-      end
-  end
+	protect_from_forgery with: :null_session
+	def recibir
+		url_api_oc = "https://dev.oc.2021-1.tallerdeintegracion.cl/oc"
+		# se recibe el id de la orden de compra
+		oc_id = params[:oc_id]
+		# se revisa que no este duplicada FALTA
+		if OrdenCompraRecibida.exists?(oc_id: params[:oc_id])
+			oc =OrdenCompraRecibida.find_by(oc_id: params[:oc_id])
+			render json: {mensaje: "OC ya fue recibida"},status: :bad_request
+		# Si esta duplicada se responde con un error 400 Bad request y con un json con {"mensaje": "OC ya fue recibida"}
+		# obtenemos url y se hace el request
+	  	else
+		  url = url_api_oc + "/obtener/" + oc_id
+		  @oc_json = HTTParty.get(url).parsed_response
+		  ## se guarda la informacion en la bd
+		  fecha_entrega = Time.at(oc_json['fechaEntrega'].to_i/1000)
+		  creada_en = Time.now
+		  # ejemplo para crear instancias en un modelo: artist = Artist.create(artist_id: artist_id, name: artist_name, age: artist_age, albums_url: albums_url, tracks_url: tracks_url, self: self_url)
+		  oc_nueva = OrdenCompraRecibida.create(oc_id: oc_json['id'], cliente: oc_json['cliente'] , sku: oc_json['sku'], fechaEntrega: fecha_entrega, 
+			  cantidad: oc_json['cantidad'], urlNotificacion: oc_json['urlNotificacion'], estado: "recibida", creacion: creada_en , en_despacho: false)
+		  # se guadrda y se hace el response
+		  if oc_nueva.save
+			oc = OrdenCompraRecibida.find_by(oc_id: params[:oc_id])
+			render json: {id: oc.oc_id, 
+			  cliente: oc.cliente, 
+			  sku: oc.sku, 
+			  fechaEntrega: oc.fechaEntrega.to_i,
+			  cantidad: oc.cantidad,
+			  urlNotificacion: oc.urlNotificacion,
+			  estado: oc.estado},status: :created
+		  else
+			render status: :bad_request
+		  end
+	  	end
+	  	
+	end
 
   def index
   end
@@ -65,9 +64,9 @@ class OrdenCompraController < ApplicationController
 	url_api = "https://dev.oc.2021-1.tallerdeintegracion.cl/oc/"
 	#falta decidir que pasa cuando no vienen los parámetros correctos
 	#Revisar si existe registro de este id en nuestra BD  
-	oc = Orden_compra_recibida.find_by(oc_id: params[:oc_id])
+	oc = OrdenCompraRecibida.find_by(oc_id: params[:oc_id])
 	if oc == nil 
-		render status: 404   #no existe id con estos datos en nuestra BD
+		render json: {"mensaje": "No hay registros con ese ID"}, status: 404   #no existe id con estos datos en nuestra BD
 	else
 		if decision == "aceptada"
 			#avisamos a la API OC
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   ## ORDENES DE COMPRA
-  post 'orden_compra/:oc_id', to: 'orden_compra#recibir'
+  post 'ordenes-compra/:oc_id', to: 'orden_compra#recibir'
   get 'orden_compra/new'
   get 'orden_compra/index'
   get 'orden_compra/show'
-  patch 'orden_compra/:oc_id', to: 'orden_compra#actualizar_estado'
+  patch 'ordenes-compra/:oc_id', to: 'orden_compra#actualizar_estado'
   ## STOCKS
   get 'stocks', to: 'stock#index'
   get 'stock/show'
